17d2b58e9d1af8091750b3bebce8db58e18b608f
Souhaïl Bechchari <146872818+SouhailBechchari@users.noreply.github.com>
2023-10-08 11:28:08 +0100

    Add files via upload

---
+++ ex14.py
@@ -0,0 +1,9 @@
+A = int(input(" tapez votre age"))
+if A == 6 or A == 7 :
+    print("poussin")
+if A == 8 or A == 9 :
+    print("pupile")
+if A == 10 or A == 11 :
+    print("minime")
+if A >= 12 :
+     print("cadet")
